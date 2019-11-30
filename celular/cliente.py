aeac40566d348a59632b0ea5709fd181bf5d3bf7
GAbrielDantasdds <50758308+GAbrielDantasdds@users.noreply.github.com>
2019-11-29 22:16:08 -0400

    Update cliente.py
    Arrumando a casa

--- celular/cliente.py
+++ celular/cliente.py
@@ -1,7 +1,7 @@
 import socket
 
 def enviar(dado):
-    ip = '192.168.0.11'
+    ip = 'endereço de ip da sua máquina'
     port = 7000 
     addr = ((ip,port)) 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
